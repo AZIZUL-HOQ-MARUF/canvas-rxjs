a905a149af698023c8c10c9de3482dc014871b50
AZIZUL-HOQ-MARUF <42655898+AZIZUL-HOQ-MARUF@users.noreply.github.com>
2022-09-27 15:41:21 +0600

    3 files modified

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import './style.css';
 
 import { fromEvent, skip } from 'rxjs';
-import { switchMap, repeat, takeUntil, scan } from 'rxjs/operators';
+import { switchMap, repeat, takeUntil, scan, map } from 'rxjs/operators';
 
+let lineWidth = 2;
 let canvas: HTMLCanvasElement = document.getElementById(
   'canvas'
 ) as HTMLCanvasElement;
@@ -19,9 +20,9 @@ $mouseDown
   .pipe(
     switchMap((down) => $mouseMove),
     takeUntil($mouseUp),
-    scan((acc, event) => ({ prev: acc.curt, curt: event }), {
+    scan((acc, event) => ({ prev: acc.cuurentTarget, cuurentTarget: event }), {
       prev: null,
-      curt: null,
+      cuurentTarget: null,
     }),
     skip(1),
     repeat()
@@ -29,13 +30,13 @@ $mouseDown
   .subscribe((data) => {
     let prevX = data.prev.clientX - offsetX;
     let prevY = data.prev.clientY - offsetY;
-    let curtX = data.curt.clientX - offsetX;
-    let curtY = data.curt.clientY - offsetY;
+    let cuurentTargetX = data.cuurentTarget.clientX - offsetX;
+    let cuurentTargetY = data.cuurentTarget.clientY - offsetY;
 
     context.beginPath();
     context.moveTo(prevX, prevY);
-    context.lineTo(curtX, curtY);
-    context.lineWidth = 5;
+    context.lineTo(cuurentTargetX, cuurentTargetY);
+    context.lineWidth = lineWidth;
     context.stroke();
     context.closePath();
   });
@@ -46,3 +47,10 @@ let $clearClicked = fromEvent(clearButton, 'click');
 $clearClicked.subscribe(() =>
   context.clearRect(0, 0, canvas.width, canvas.height)
 );
+
+let size = document.getElementById('size');
+let $sizeChanged = fromEvent(size, 'change');
+
+$sizeChanged
+  .pipe(map((event) => (event.target as HTMLInputElement).value))
+  .subscribe((value) => (lineWidth = +value || lineWidth));
